645a11076e0903bae5608d47c698e9b705d43d10
Brian <Bhensley811@gmail.com>
2023-01-11 12:46:56 -0500

    commit tree 1fdbc825811155ddf2e234d0327de0bd7db55a82

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// function findMatching(drivers){
+//     const correctDrivers = drivers.filter((drivers) => drivers === ['Bobby'])
+    
+//     return correctDrivers
+// }
+function findMatching(earth, fire) {
+    return earth.filter((water) => water.toLowerCase().includes(fire.toLowerCase()))
+}
+
+function fuzzyMatch(earth, fire) {
+    return earth.filter((water) => water.toLowerCase().includes(fire.toLowerCase()))
+}
+
+function matchName(earth, fire){
+    return earth.filter((water) => water.toLowerCase().includes(fire.toLowerCase()))
+}
